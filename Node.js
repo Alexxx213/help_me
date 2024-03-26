aa0987fe9de4470fc3b909f07db9593d615d71c9
AlexandrOkunev <alex2002493@gmail.com>
2024-03-27 01:01:41 +0300

    Added footer

---
+++ Node.js
@@ -0,0 +1,33 @@
+
+
+
+
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+  console.log('Server request');
+
+  res.setHeader('Content-Type', 'text/html');
+
+  if(req.url='/'){
+   fs.readFile('./views/index.html', (err, data) => {
+     if (err) {
+       console.log(err);
+       res.statusCode = 500;
+       res.end();
+    }
+    else {
+       res.write(data);
+       res.end();
+    }
+   });
+  }
+});
+
+server.listen(PORT, 'localhost', (error) => {
+  error ? console.log(error) : console.log(`listening port ${PORT}`);
+});
